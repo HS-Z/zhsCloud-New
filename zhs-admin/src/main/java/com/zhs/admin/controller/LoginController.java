2a6236d4987ffe1409c7f6a9f09ecf054011b026
hs-z <www.aiwoo@live.com>
2019-08-21 23:41:59 +0800

    login

--- zhs-admin/src/main/java/com/zhs/admin/controller/LoginController.java
+++ zhs-admin/src/main/java/com/zhs/admin/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.zhs.admin.controller;
 
+import com.zhs.common.model.Json;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Zhang on 2019/8/12.
@@ -20,6 +23,17 @@ public class LoginController {
     @RequestMapping(value = "index",method = RequestMethod.GET)
     public String index(){
         return "index";
+    }
+
+
+    @RequestMapping(value = "loginSystem",method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public Json loginSystem(@RequestParam("account") String account, @RequestParam("password") String password){
+        try {
+            return Json.ok();
+        }catch (Exception e){
+            return Json.fail("登陆失败，请联系管理员！");
+        }
     }
 
 
--- zhs-common/src/main/resources/static/aa/js/good.js
+++
@@ -1,7 +0,0 @@
-$("#kk").click(function () {
-    alert("你好");
-});
-
-function aa() {
-    alert("什么鬼");
-}
