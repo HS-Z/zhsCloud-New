0ea2dc5f7b375d7333f3c0b85f0db59058a19d19
hs-z <www.aiwoo@live.com>
2019-09-24 23:02:44 +0800

    日期工具类

--- zhs-common/src/main/java/com/zhs/common/utils/DateUtils.java
+++ zhs-common/src/main/java/com/zhs/common/utils/DateUtils.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
-import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Created by Zhang on 2019/8/12.
@@ -26,7 +24,7 @@ public class DateUtils {
      * 获取时间戳（豪秒）
      * @return  13位数字
      */
-    public static Long getTimeStampWithMillisecond(){
+    public static Long getTimeStampWithMilliSecond(){
         return LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
     }
 
@@ -42,13 +40,37 @@ public class DateUtils {
 
 
     /**
+     * 获取当前时间，精确到天（中文格式）
+     * @return  2019年9月24日
+     */
+    public static String getNowDateChinese(){
+        LocalDate localDate = LocalDate.now();
+        String date = localDate.getYear() + "年" + localDate.getMonthValue() + "月" + localDate.getDayOfMonth() + "日";
+        return date;
+    }
+
+
+    /**
      * 获取当天时间，不含具体的年月日，精确到毫秒
-     * @return  00:07:50.251
+     * @return  22:23:01.926
      */
     public static String getDayTime(){
         LocalTime localTime = LocalTime.now();
         return localTime.toString();
     }
+
+
+    /**
+     * 获取今天是今年的第多少天
+     * @return  267
+     */
+    public int getDayNumberInYear(){
+        LocalDate localDate = LocalDate.now();
+        return localDate.getDayOfYear();
+    }
+
+
+
 
 
 
